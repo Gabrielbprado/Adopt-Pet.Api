6a89197219def558be0f9bfc6454d0d49bbc1394
Gabriel <guardavege@gmail.com>
2024-01-24 15:56:02 -0300

    Refatorando Rotas

---
+++ Models/TutorModel - Cópia.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Adopt_Pet.Api.Models
+{
+    public class AbrigoModel : IdentityUser
+    {
+        public string City { get; set; } = null!;
+
+        public string State { get; set; } = null!;
+        public TutorModel() : base() { }
+     
+    }
+}
