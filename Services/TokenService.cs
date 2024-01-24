80673963725b71674065d539277bd67b9beceb3f
Gabriel <guardavege@gmail.com>
2024-01-24 15:44:34 -0300

    Token JWT Created

---
+++ Services/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using Adopt_Pet.Api.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Adopt_Pet.Api.Services;
+
+public class TokenService
+{
+    private IConfiguration _configuration;
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration  = configuration;
+    }
+
+    public string GenerateToken(TutorModel model)
+    {
+        Claim[] claims = new Claim[]
+        {
+              new Claim("id",model.Id),
+              new Claim("username",model.UserName),
+              new Claim("city",model.City),
+              new Claim("state",model.State),
+              new Claim("email",model.Email),
+              new Claim("phoneNumber",model.PhoneNumber),
+               
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("asdadauiohdiauydiisa65"));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(
+            expires: DateTime.Now.AddDays(30),
+            claims: claims,
+            signingCredentials: creds
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+            
+    }
+}
